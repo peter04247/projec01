e82e6e0eefb84a46388a628ac892e030333adabb
peter04247 <75821891+peter04247@users.noreply.github.com>
2024-08-12 19:51:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import pandas as pd
-import streamlit as sl
+import streamlit as st
 import numpy as np
 
-
-dt = {
-  'cars': ['BMW', 'Volvo', 'Ford'],
-  'passings': [3, 7, 2]
-}
-
-df = pd.DataFrame(dt)
+st.title("Uber pickup in NYC")
