8b395cdcb55235abf472f0ae5cdd42a44b359270
peter04247 <75821891+peter04247@users.noreply.github.com>
2024-08-19 19:15:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
-
+st.title('Uber pickups in NYC - by Peter')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -18,20 +16,21 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+data_load_state.text('Loading data...done!')
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+st.subheader('Raw data')
+st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.subheader("Number of pickups by hour")
+
+hist_values = np.histogram(
+  data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
 st.bar_chart(hist_values)
 
-# Some number in the range 0-23
+
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
 
